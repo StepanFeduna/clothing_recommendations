0c521bf088231c70220858109f558ada8b70a8ab
stepan_feduna <feduna.stepan@gmail.com>
2022-08-05 16:36:46 +0300

    Create FastAPI template

--- main.py
+++ main.py
@@ -4,9 +4,25 @@ This module holds the back-end logic on how to deploy a TensorFlow model as a RE
 
 import os
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from uvicorn import run
+import uuid
+
+import numpy as np
+import torch
+import cv2
+import os
+import pickle
+import numpy as np
+import json
+import tensorflow as tf
+from keras.preprocessing import image
+from keras.applications.mobilenet_v3 import preprocess_input
+from keras.models import Model
+
+IMAGEDIR = "fastapi-images/"
 
 app = FastAPI()
 
@@ -22,8 +38,34 @@ app.add_middleware(
     allow_headers=headers,
 )
 
-# model_dir = "clothing-recommendation-model.h5"
-# model = load_model(model_dir)
+
+IMAGE_WIDTH = 224
+IMAGE_HEIGHT = 224
+IMAGE_SIZE = (IMAGE_WIDTH, IMAGE_HEIGHT)
+
+
+def get_embedding(model, imagename):
+    img = image.load_img(imagename, target_size=IMAGE_SIZE)
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return model.predict(x).reshape(-1)
+
+
+restored_model = tf.keras.models.load_model("model/bestmodel.h5")
+secondmodel = Model(
+    inputs=restored_model.input, outputs=restored_model.layers[-4].output
+)
+# print(secondmodel.summary())
+restored_model.compile(
+    loss="categorical_crossentropy", optimizer="nadam", metrics=["accuracy"]
+)
+secondmodel.compile(
+    loss="categorical_crossentropy", optimizer="nadam", metrics=["accuracy"]
+)
+yolomodel = torch.hub.load(
+    "ultralytics/yolov5", "custom", path="yolov5/runs/train/Model/weights/best.pt"
+)
 
 
 @app.get("/")
@@ -32,11 +74,51 @@ async def root():
     return {"message": "Welcome to the Clothing Recommendation API!"}
 
 
-@app.post("/net/image/recommendation/")
-async def get_net_image_prediction(image_link: str = ""):
-    """Main API functionality"""
-    if image_link == "":
-        return {"message": "No image link provided"}
+@app.post("/images/")
+async def create_upload_file(file: UploadFile = File(...)):
+    """Upload image file"""
+    file.filename = f"{uuid.uuid4()}.jpg"
+    contents = await file.read()  # <-- Important!
+
+    # save the file
+    with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
+        f.write(contents)
+
+    return {"filename": file.filename}
+
+
+@app.get("/images/")
+async def read_upload_file():
+    """Display uploaded filey"""
+    files = os.listdir(IMAGEDIR)
+
+    path = f"{IMAGEDIR}{files[-1]}"
+
+    return FileResponse(path)
+
+
+@app.delete("/images/")
+async def delete_upload_file():
+    """delete uploaded image"""
+    pass
+
+
+@app.get("/images/")
+async def clothes_detection():
+    """Search for clothes on image"""
+    pass
+
+
+@app.post("/images/")
+async def choose_clothes():
+    """Choose which clothes user want to find"""
+    pass
+
+
+@app.get("/images/")
+async def recommend_clothes():
+    """Recomend similar clothes"""
+    pass
 
 
 if __name__ == "__main__":
