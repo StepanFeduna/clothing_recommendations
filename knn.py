21da441da524af817505f7c38751a89a9eedb224
stepan_feduna <feduna.stepan@gmail.com>
2022-08-10 23:15:25 +0300

    Run the (K-NN) on numarray of crawled images

--- knn.py
+++ knn.py
@@ -1,72 +1,89 @@
 """
-Run the similarity calculation algorithm (K-NN)
+Run the similarity calculation algorithm (K-NN) on numarray of crawled images 
 """
 
 import tensorflow as tf
-import json
 import pandas as pd
-from keras.preprocessing import image
-import numpy as np
 from sklearn.neighbors import NearestNeighbors
 import pickle
 from keras.models import Model
 
-
-IMAGE_WIDTH = 224
-IMAGE_HEIGHT = 224
-IMAGE_SIZE = (IMAGE_WIDTH, IMAGE_HEIGHT)
-IMAGE_CHANNELS = 3
+from sqlmodel import Session, select
+from database.database import create_db_and_tables, fill_table, engine
+from database.db_tables import ResNet50v2Model, CrawlData, KnnModel, ClothesCategory
 
 
-with open("datasets/notedarray.json", "r") as f:
-    temp = json.loads(f.read())
-    for i in temp:
-        if i == "item":
-            continue
-        else:
-            imagelink = []
-            categories = []
-            notedarray = []
-            for s in range(len(temp[i])):
-                imagelink.append(str(temp[i][s]["imagelink"]))
-                categories.append(str(temp[i][s]["categories"]))
-                fixarray = str(temp[i][s]["notedarray"]).replace("[", "")
-                fixarray = fixarray.replace("]", "")
-                notedarray.append(fixarray.split(","))
-                df = pd.DataFrame(
-                    {
-                        "imagelink": imagelink,
-                        "category": categories,
-                        "notedarray": notedarray,
-                    }
-                )
-    # print(df)
+def sql_reader(engine, query):
+    """Read SQL query into a DataFrame."""
 
-restored_model = tf.keras.models.load_model("model/bestmodel.h5")
-restored_model.compile(
-    loss="categorical_crossentropy",
-    optimizer="nadam",
-    metrics=["accuracy"],
-)
-secondmodel = Model(
-    inputs=restored_model.input, outputs=restored_model.layers[-4].output
-)
-secondmodel.compile(
-    loss="categorical_crossentropy",
-    optimizer="nadam",
-    metrics=["accuracy"],
-)
-clothes_categories = ["shirt", "outwear", "short", "skirt", "dress"]
+    return pd.read_sql_query(query, con=engine)
 
-for i in range(len(clothes_categories)):
-    select = df.loc[df["category"] == clothes_categories[i - 1]]
-    map_embeddings = select["notedarray"]
-    print(map_embeddings)
-    df_embs = map_embeddings.apply(pd.Series)
-    print(df_embs.shape)
-    neighbors = NearestNeighbors(n_neighbors=5, algorithm="brute", metric="euclidean")
-    neighbors.fit(df_embs)
-    select = select.iloc[0:0]
-    # print(select)
-    knnPickle = open("datasets/crawldata/" + clothes_categories[i - 1] + "/knn.pkl", "wb")
-    pickle.dump(neighbors, knnPickle)
+
+def read_sql():
+    """Returns a DataFrame corresponding to the result set of the query string."""
+
+    with Session(engine) as session:
+        statement = select(
+            CrawlData.notedarray, ClothesCategory.generic_category
+        ).join_from(
+            CrawlData,
+            ClothesCategory,
+            CrawlData.category == ClothesCategory.extended_category,
+        )
+        data_table = session.exec(statement).all()
+
+    crawl_data = pd.DataFrame.from_records(data_table, columns=["notedarray", "category"]).dropna()
+
+    print(crawl_data.head(5))
+
+    return crawl_data
+
+
+def load_model():
+    """Load model file from it's path link in DB"""
+    
+    with Session(engine) as session:
+        statement = select(ResNet50v2Model.best_model)
+        best_model = session.exec(statement).first()
+
+    restored_model = tf.keras.models.load_model(best_model)
+    restored_model.compile(
+        loss="categorical_crossentropy", optimizer="nadam", metrics=["accuracy"]
+    )
+
+    # Reuse last activation layer to extract image characteristics
+    secondmodel = Model(
+        inputs=restored_model.input, outputs=restored_model.layers[-4].output
+    )
+    secondmodel.compile(
+        loss="categorical_crossentropy", optimizer="nadam", metrics=["accuracy"]
+    )
+
+    return secondmodel
+
+
+def gen_knn_model():
+    """Generate results of KNN model calculations"""
+
+    crawl_data = read_sql()
+    categories_set = crawl_data["category"].unique()
+    for category in categories_set:
+        df_select = crawl_data.loc[crawl_data["category"] == category]
+        # print(df_select.head(1))
+        map_embeddings = df_select["notedarray"]
+        # print(map_embeddings.head(1))
+        df_embs = map_embeddings.apply(pd.Series)
+        # print(df_embs.shape)
+        neighbors = NearestNeighbors(
+            n_neighbors=len(categories_set), algorithm="brute", metric="euclidean"
+        )
+        neighbors.fit(df_embs)
+        knn_path = f"knn/{category}_knn.pkl"
+        with open(knn_path, "wb") as knn_pickle:
+            pickle.dump(neighbors, knn_pickle)
+        yield {"category": category, "model": knn_path}
+
+
+if __name__ == "__main__":
+    create_db_and_tables()
+    fill_table(KnnModel, gen_knn_model(), truncate=True)
