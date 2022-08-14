4c08986afcf7fc91d8b71eba3a399f148f2c9090
stepan_feduna <feduna.stepan@gmail.com>
2022-08-14 18:32:33 +0300

    Update file structure

---
+++ project/alembic/versions/260e8a9d5082_create_account_table.py
@@ -0,0 +1,25 @@
+"""create account table
+
+Revision ID: 260e8a9d5082
+Revises: 
+Create Date: 2022-08-14 18:08:53.971292
+
+"""
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel
+
+
+# revision identifiers, used by Alembic.
+revision = '260e8a9d5082'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
